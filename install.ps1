4e1fd13f3f66bd18bce152a583c8f5e41241c718
kjarom <altkarom@gmail.com>
2020-07-07 13:23:52 +0700

    install now

--- install.ps1
+++ install.ps1
@@ -1,3 +1,11 @@
+# check to see if we are currently running 'as Administrator'
+if (!(Verify-Elevated)) {
+    $New_Process = New-Object
+
+
+
+
+
 Set-ExecutionPolicy RemoteSigned -scope CurrentUser
 iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
 
